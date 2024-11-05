0013fb71d2cfc7563bafe7be01e2ee88f39ca5c4
Aaron McIntyre <2618972m@student.gla.ac.uk>
2024-11-05 13:49:04 +0000

    All changes, artist profile implemented

--- Off_Axis_App/views.py
+++ Off_Axis_App/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Artist
 
 # Create your views here.
 
@@ -15,3 +16,10 @@ def components(request):
             ]
         },
     )
+    
+def artist_profile(request, slug):
+    artist_profile_page = get_object_or_404(Artist, slug=slug)
+    if (artist_profile_page.is_approved == True):
+        return render(request, 'artist_profile.html', {'artist_profile': artist_profile_page})
+    else:
+        return render(request, 'artist_not_approved.html', {'artist_profile': artist_profile_page})
