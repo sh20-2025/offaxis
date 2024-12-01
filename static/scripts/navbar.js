1ac43a678b5b063f539c7f1d571586335b726e29
Aaron McIntyre <2618972m@student.gla.ac.uk>
2024-12-01 23:19:44 +0000

    feat: Working mobile/desktop responsive navbar

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -1,11 +1,16 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerButton = document.getElementById('hamburgerButton');
+    const nav = document.querySelector('.nav');
+
+
+    hamburgerButton.addEventListener('click', () => {
+        nav.classList.toggle('active');
+    });
+
     const navbar = document.getElementById('navbar-header-container');
     window.addEventListener('scroll', () => {
         const scrollPosition = window.scrollY;
-        const pageHeight = document.documentElement.scrollHeight - window.innerHeight;
-        const scrollPercentage = (scrollPosition / pageHeight) * 100;
-
-        if (scrollPercentage > 5) {
+        if (scrollPosition > 50) {
             navbar.classList.add('scrolled');
         } else {
             navbar.classList.remove('scrolled');
