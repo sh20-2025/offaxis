0d34acbf8e253c332424deaea2ee703de34ecadb
Aaron McIntyre <2618972m@student.gla.ac.uk>
2024-12-03 18:18:13 +0000

    chore: Fixed changes to navbar

--- static/scripts/navbar.js
+++ static/scripts/navbar.js
@@ -1,10 +1,21 @@
 document.addEventListener('DOMContentLoaded', () => {
+    const scriptTag = document.querySelector('script[src*="navbar.js"]');
+    const staticUrl = scriptTag.getAttribute('data-static-url');
+
     const hamburgerButton = document.getElementById('hamburgerButton');
     const nav = document.querySelector('.nav');
-
+    const svgIcon = document.getElementById('hamburgerMenuSVG'); // This is an <img> element
 
     hamburgerButton.addEventListener('click', () => {
         nav.classList.toggle('active');
+
+        // Update the `src` attribute for the <img> tag
+        const currentSrc = svgIcon.getAttribute('src');
+        if (currentSrc === `${staticUrl}icons/hamburger-menu.svg`) {
+            svgIcon.setAttribute('src', `${staticUrl}icons/close-button.svg`);
+        } else {
+            svgIcon.setAttribute('src', `${staticUrl}icons/hamburger-menu.svg`);
+        }
     });
 
     const navbar = document.getElementById('navbar-header-container');
