17f3d3b21c253e4754f2ab1d1c40ce666f0c10fe
zhixitee <dicksont91@gmail.com>
2025-02-12 21:28:51 +0000

    chore: yml minor changes

--- Off_Axis_Django/settings.py
+++ Off_Axis_Django/settings.py
@@ -126,6 +126,8 @@ STATICFILES_DIRS = [STATIC_DIR]
 
 STATIC_URL = "static/"
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
