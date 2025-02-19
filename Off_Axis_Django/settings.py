bb685e23453359839ff07040c4b051c3c17b948e
zhixitee <dicksont91@gmail.com>
2025-02-19 20:00:01 +0000

    fix: settings STATIC at settings.py

--- Off_Axis_Django/settings.py
+++ Off_Axis_Django/settings.py
@@ -128,8 +128,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-# STATICFILES_DIRS = [STATIC_DIR]
-# STATIC_URL = "static/"
+STATICFILES_DIRS = [STATIC_DIR]
+STATIC_URL = "static/"
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
