41476be2f04f9b3c74ce19060788e553250e9e08
freddie nelson <freddie0208@hotmail.com>
2025-03-20 17:58:57 +0000

    feat: responsive home page

--- static/scripts/landing_page.js
+++ static/scripts/landing_page.js
@@ -1,12 +1,14 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
   const headings = document.querySelectorAll(".hero-circle h4");
-  headings.forEach(heading => {
+  headings.forEach((heading) => {
     const text = heading.textContent;
     heading.innerHTML = "";
     for (let i = 0; i < text.length; i++) {
       const span = document.createElement("span");
       span.textContent = text[i];
-      span.style.transform = `translate(-50%, -780%) rotate(${(-i * 3.2) + (text.length*1.2) + 190}deg) translateY(-160px) scale(-1, -1)`;
+      span.style.transform = `translate(-50%, -780%) rotate(${
+        -i * 3.2 + text.length * 1.2 + 190
+      }deg) translateY(-160px) scale(-1, -1)`;
       span.style.display = "inline-block";
       span.style.position = "absolute";
       span.style.transformOrigin = "bottom center";
@@ -14,4 +16,9 @@ document.addEventListener("DOMContentLoaded", function() {
       heading.appendChild(span);
     }
   });
+
+  const instaCarousel = document.querySelector(".insta-carousel");
+  if (instaCarousel) {
+    instaCarousel.style.setProperty("--insta-count", instaCarousel.children.length);
+  }
 });
